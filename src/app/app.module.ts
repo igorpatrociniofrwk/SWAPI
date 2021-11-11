74c3338792f0e82e801526ab1252d996f30f2c74
igorpatrociniofrwk <igorpatrocinio@frwk.com.br>
2021-11-11 13:08:43 -0300

    3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,9 +16,7 @@ import { SpeciesComponent } from './species/species.component';
 import { StarshipsComponent } from './starships/starships.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
 import { LoginComponent } from './login/login.component';;
-import { Login2Component } from './login2/login2.component';
-import { RouterModule } from '@angular/router';
-;
+import { Login2Component } from './login2/login2.component';;
 @NgModule({
   declarations: [
     AppComponent,
@@ -34,10 +32,10 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     NgbModule,
-    HttpClientModule,
-    RouterModule
+    HttpClientModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
