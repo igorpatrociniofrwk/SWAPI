ba797e4c7b9f5477c47156ebac7e34eb9f941c89
igorpatrociniofrwk <igorpatrocinio@frwk.com.br>
2021-11-26 14:18:07 -0300

    atualizacao de responsividade

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService {
 
   fazerLogin(usuario: Usuario){
 
-    if (usuario.nome === 'usuario@email.com' &&
+    if (usuario.email === 'usuario@email.com' &&
       usuario.senha === '123456') {
 
         this.usuarioAutenticado = true;
@@ -23,8 +23,8 @@ export class AuthService {
 
         this.router.navigate(['/entrar'])
       } else{
+        alert("Email ou senha incorretos.");
         this.usuarioAutenticado = false;
-
         this.mostrarMenuEmitter.emit(false);
       }
   }
